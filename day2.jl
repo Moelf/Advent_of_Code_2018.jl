37d344ff6443ab0514cca38ef024dd721b0d5edb
Moelf <jerryling315@gmail.com>
2018-12-01 23:46:29 -0800

    Add Day2 challenge

--- day2.jl
+++ day2.jl
@@ -1,15 +1,38 @@
-r = open("C:\\Users\\jerry\\Documents\\GitHub\\Advent\\day1.txt")
+r = open("./day2.txt")
 r = readlines(r)
-t = 0
-t_list = [0]
-for ln in r
-    global t_list
-    global t
-    a = eval(Meta.parse("0" * ln))
-    t+=a
-    if t in t_list
-        print("Fount $t.")
-        break
+
+# Part 1
+twos = 0
+threes = 0
+for eachLabel in r
+    u = unique(eachLabel)
+    freq = []
+    for letter in u
+        push!(freq, count(l->l==letter, eachLabel))
     end
-    push!(t_list, t)
+    if 2 in freq
+        global twos += 1
+    end
+    if 3 in freq
+        global threes +=1
+    end
 end
+#= println(twos * threes) =#
+
+#= Part 2 =#
+
+for i in 1:length(r)
+    for j in i:length(r)
+        diffCount = 0
+        for letterIndex in 1:26
+            if r[i][letterIndex] != r[j][letterIndex]
+                diffCount += 1
+            end
+        end
+        if diffCount == 1
+            println(r[i])
+            println(r[j])
+        end
+    end
+end
+
